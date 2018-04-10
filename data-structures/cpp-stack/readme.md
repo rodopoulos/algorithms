035c88e3e282f986f48f438f5d060147734e2e22
Felipe Rodopoulos <felipekss@gmail.com>
2018-04-10 10:34:06 -0300

    stack basic implementation

---
+++ data-structures/cpp-stack/readme.md
@@ -0,0 +1,4 @@
+# Stacks
+Stacks are a data structure that follows the Last In, First Out (LIFO) principle. Hence, if we insert `n` elements in increasing order, i.e., `a1, a2, ..., an`, they must be extracted from the stack in the reversed order: `an, ..., a2, a1`.
+
+## Operations
---
+++ data-structures/cpp-stack/stack.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+struct node {
+  node<T> *next;
+  T el;
+};
+
+template <class T>
+class stack {
+  private:
+    int _size;
+    node<T> *s;
+
+  public:
+    stack();
+    void push(T elem);
+    T top();
+    T pop();
+    void print();
+
+    int size() {
+      return _size;
+    }
+
+    bool empty() {
+      return _size == 0;
+    }
+};
+
+template <class T> stack<T>::stack(){
+  _size = 0;
+}
+
+template <class T> void stack<T>::push(T el) {
+  node<T> *p;
+  p = new node<T>;
+  p->el = el;
+  if (s) {
+    p->next = s;
+  }
+  s = p;
+  _size++;
+}
+
+template <class T> T stack<T>::pop() {
+  node<T> *p;
+  p = s;
+  T r = s->el;
+  s = p->next;
+  delete[] p;
+  _size--;
+
+  return r;
+}
+
+template <class T> T stack<T>::top() {
+  return s->el;
+}
+
+template <class T> void stack<T>::print() {
+  node<T> *p;
+  p = s;
+  while (p) {
+    cout << p->el << " ";
+    p = p->next;
+  }
+  cout << "#\n";
+}
+
+
+int main() {
+  stack<int> s1;
+  s1.print();
+  s1.push(1);
+  s1.print();
+  s1.push(2);
+  s1.print();
+  s1.push(3);
+  s1.print();
+  cout << "Size:" << s1.size() << "\n";
+  cout << "Top:" << s1.top() << "\n";
+
+  s1.print();
+  cout << s1.pop() << "\n";
+  s1.print();
+  cout << s1.pop() << "\n";
+  s1.print();
+  cout << s1.pop() << "\n";
+  cout << "Size:" << s1.size() << "\n";
+
+  return 0;
+}
