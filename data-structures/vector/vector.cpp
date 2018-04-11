ba07fa37326b7b91717387f26e81af70c44ffbac
Felipe Rodopoulos <felipekss@gmail.com>
2018-04-10 23:36:51 -0300

    initial vector implementation

---
+++ data-structures/vector/vector.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+struct node {
+  node<T> *next;
+  T el;
+};
+
+template <class T>
+class vector {
+  private:
+    static const int INITIALSIZE = 5;
+    int _size, _cap;
+    T *v;
+
+    void realocate();
+
+  public:
+    vector();
+    vector(int sz);
+    ~vector();
+    void insert(T elem);
+    void erase(int pos);
+    T at(int pos);
+    void print();
+
+    int size() {
+      return _size;
+    }
+
+    bool empty() {
+      return _size == 0;
+    }
+};
+
+template <class T> void vector<T>::realocate() {
+  T *n = new T[_cap * 2];
+  memcpy(n, v, _size * sizeof(T));
+  _cap = _cap << 2;
+  delete[] v;
+  v = n;
+}
+
+template <class T> vector<T>::vector(){
+  _size = 0;
+  _cap = 5;
+  v = new T[_cap];
+}
+
+template <class T> vector<T>::vector(int sz){
+  _size = 0;
+  _cap = sz;
+  v = new T[_cap];
+}
+
+template <class T> vector<T>::~vector() {
+  delete[] v;
+}
+
+template <class T> void vector<T>::insert(T el) {
+  if (_size + 1 > _cap) {
+    realocate();
+  }
+  v[_size] = el;
+  _size++;
+}
+
+template <class T> void vector<T>::erase(int pos) {
+  for (int i = pos; i < _size-1; i++) {
+    v[i] = v[i+1];
+  }
+  _size--;
+}
+
+template <class T> T vector<T>::at(int pos) {
+  return v[pos];
+}
+
+template <class T> void vector<T>::print() {
+  cout << "[ ";
+  for(int i = 0; i < _size; i++) {
+    cout << v[i] << " ";
+  }
+  cout << "]\n";
+}
+
+
+int main() {
+  vector<int> v1;
+  v1.print();
+  v1.insert(1);
+  v1.insert(2);
+  v1.insert(3);
+  v1.insert(4);
+  v1.print();
+  v1.insert(5);
+  v1.print();
+  v1.insert(6);
+  v1.print();
+  v1.erase(1);
+  v1.print();
+
+  return 0;
+}
