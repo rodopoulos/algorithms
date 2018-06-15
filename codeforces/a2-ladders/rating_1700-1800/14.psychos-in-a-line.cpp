9ce3fbfaa34cb2427b189069cb66a29ad159870c
Felipe Rodopoulos <felipekss@gmail.com>
2018-06-15 13:25:15 -0300

    updating solution for psychos problems

--- codeforces/a2-ladders/rating_1700-1800/14.psychos-in-a-line.cpp
+++ codeforces/a2-ladders/rating_1700-1800/14.psychos-in-a-line.cpp
@@ -1,22 +1,26 @@
 /* Problem B - Round #189 - Div. 1
  * http://codeforces.com/problemset/problem/319/B
  *
+ * SOLUTION OVERVIEW
+ * First, we find all the psychos who will survive at the end, marking them in
+ * auxiliary array. Now, we need to find the psycho who will take the greater
+ * time to die, updating our answer in case this time is greater than the
+ * previous one. So, we loop again over the input, doing the same computating on
+ * the stack for finding who will last at the end, but at each a[i], we do:
  *
+ * a) If a[i] will survive at the end, he will never die and thus, his death time
+ *    is 0. Now, it is important to add fim at the end of our stack.
+ *
+ * b) If a[i] will not survive, he will eventually die. At this time, j indicates
+ * how many psychos will survive that are place on the right of a[i] and
+ * s.size() - j will be the number of psychos that will die by a[i]. This is the
+ * number that interest us for comparing with previous computations.
 */
 #include <bits/stdc++.h>
 using namespace std;
 
 int psy[100001];
 bool surv[100001];
-
-void printstack(stack<int> s) {
-  cout << "Stack: ";
-  while (!s.empty()){
-    cout << s.top() << ' ';
-    s.pop();
-  }
-  cout << "]" << endl;
-}
 
 int main() {
   int n;
@@ -39,24 +43,17 @@ int main() {
 
   int j = 0, ans = 0;
   for (int i = n; i >= 1; i--) {
-    cout << "== PSYCHO ID " << psy[i] << endl;
-
-    printstack(s);
     while (!s.empty() && psy[i] > s.top()) {
       s.pop();
     }
     s.push(psy[i]);
-    printstack(s);
 
     if (surv[psy[i]])
       j = s.size();
     ans = max(ans, (int)s.size() - j);
-    cout << "J: " << j << endl;
-    cout << "s.size(): " << s.size() << endl;
-    cout << "ANS: " << ans << endl << endl;
   }
 
-  cout << "Final ans: " << ans << endl;
+  cout << ans << endl;
 
   return 0;
 }
